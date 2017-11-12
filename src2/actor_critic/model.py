Fixed exploding gradients. Added debugging info.

src2/actor_critic/model.py
@@ -5,7 +5,7 @@ from src2.actor_critic.utils import cat_entropy, mse, Scheduler
 
 class Model(object):
     def __init__(self, ob_space, ac_space, batch_size, ent_coef=0.01, vf_coef=0.5, max_grad_norm=0.5, lr=7e-4,
-                 alpha=0.99, epsilon=1e-5, lrschedule='linear', training_timesteps=int(80e6)):
+                 alpha=0.99, epsilon=1e-5, lrschedule='linear', training_timesteps=int(80e6), verbose=0):
         config = tf.ConfigProto(allow_soft_placement=True)
         config.gpu_options.allow_growth = True
 
@@ -20,11 +20,30 @@ class Model(object):
         step_model = CnnPolicy(sess, ob_space, n_act, 1, reuse=False)
         train_model = CnnPolicy(sess, ob_space, n_act, batch_size, reuse=True)
 
-        cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=train_model.pi, labels=A)
-        pg_loss = tf.reduce_mean(ADV * cross_entropy)
+        logits = train_model.logits
+        if verbose == 2:
+            ADV_print = tf.Print(ADV, [ADV], summarize=8, message='advs: ')
+            A_print = tf.Print(A, [A], summarize=8, message='actions: ')
+            logits = tf.Print(logits, [logits], summarize=8, message='logits: ')
+        else:
+            A_print = A
+            ADV_print = ADV
+
+        cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=A_print)
+        pg_loss = tf.reduce_mean(ADV_print * cross_entropy)
         vf_loss = tf.reduce_mean(mse(tf.squeeze(train_model.vf), R))
-        entropy = tf.reduce_mean(cat_entropy(train_model.pi))
-        loss = pg_loss - entropy * ent_coef + vf_loss * vf_coef
+        entropy = tf.reduce_mean(cat_entropy(train_model.logits))
+        reg_loss = tf.losses.get_regularization_losses()
+
+        if verbose == 2:
+            pg_loss = tf.Print(pg_loss, [pg_loss], message='pg loss: ')
+            vf_loss = tf.Print(vf_loss, [vf_loss], message='vf loss: ')
+            reg_loss = tf.Print(reg_loss, [reg_loss], message='reg loss: ')
+
+        loss = pg_loss - entropy * ent_coef + vf_loss * vf_coef + reg_loss
+
+        if verbose == 2:
+            loss = tf.Print(loss, [loss], message='loss: ')
 
         params = tf.trainable_variables(scope='AgentCriticNetwork')
         grads = tf.gradients(loss, params)
@@ -33,6 +52,7 @@ class Model(object):
         grads = list(zip(grads, params))
 
         trainer = tf.train.RMSPropOptimizer(learning_rate=LR, decay=alpha, epsilon=epsilon)
+        # trainer = tf.train.GradientDescentOptimizer(learning_rate=LR)
         _train = trainer.apply_gradients(grads)
 
         lr = Scheduler(v=lr, nvalues=training_timesteps, schedule=lrschedule)
@@ -48,7 +68,6 @@ class Model(object):
             )
 
             return policy_loss, value_loss, policy_entropy
-
 
         self.train = train
         self.train_model = train_model

src2/actor_critic/policy_network.py
@@ -16,7 +16,7 @@ def res_block(x, scope):
 
 class CnnPolicy:
     def __init__(self, sess, ob_space, n_act, n_batch, reuse=False):
-        ob_shape = (n_batch, ) + ob_space.shape
+        ob_shape = (n_batch,) + ob_space.shape
         X = tf.placeholder(tf.float32, ob_shape, name='state')
         with tf.variable_scope('AgentCriticNetwork', reuse=reuse):
             with slim.arg_scope(
@@ -33,7 +33,8 @@ class CnnPolicy:
 
                 flat = slim.flatten(res3)
                 fc = slim.fully_connected(flat, 256, scope='fc1')
-                probs = slim.fully_connected(fc, n_act, scope='logits', activation_fn=tf.nn.softmax)
+                logits = slim.fully_connected(fc, n_act, scope='logits', activation_fn=None)
+                probs = tf.nn.softmax(logits, name='probs')
                 vf = slim.fully_connected(fc, 1, scope='vf', activation_fn=tf.nn.tanh)
 
         v0 = vf[:, 0]
@@ -47,8 +48,8 @@ class CnnPolicy:
             return sess.run(v0, {X: ob})
 
         self.X = X
+        self.logits = logits
         self.pi = probs
         self.vf = vf
         self.step = step
         self.value = value
-