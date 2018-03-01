4a140f3e3fa5162d8896b1416474293250826e1d
mizworski <m.izworski7@gmail.com>
2018-03-01 19:34:54 +0100

    Extracted new functions from existing code.

--- src/actor_critic/self_play.py
+++ src/actor_critic/self_play.py
@@ -4,22 +4,9 @@ from src.actor_critic.mcts import MCTS
 from src.environment.PaperSoccer import Soccer
 from src.actor_critic.utils import ReplayMemory
 
-
-def select_action(probs, legal_moves, n_act, temperature=None):
-    if temperature is None:
-        probs_only_legal = np.multiply(probs, legal_moves)
-        return np.argmax(probs_only_legal)
-
-    probs = [prob ** (1 / temperature) for prob in probs]
-    probs_only_legal = np.multiply(probs, legal_moves)
-    l1_norm = np.linalg.norm(probs_only_legal, ord=1)
-    if l1_norm == 0:
-        probs_fixed = probs / np.linalg.norm(probs, ord=1)
-    else:
-        probs_fixed = probs_only_legal / l1_norm
-
-    return np.random.choice(range(n_act), p=probs_fixed)
-
+def playing_progress_bar(game, n_games):
+    if (game + 1) % (n_games // 10) == 0:
+        print("Completed {}% of self-play.".format(int(100 * (game + 1) / n_games)))
 
 class Runner(object):
     def __init__(self, initial_model, n_replays, c_puct):
@@ -29,37 +16,16 @@ class Runner(object):
         self.c_puct = c_puct
 
     def run(self, n_games=int(1e4), temperature=1, n_rollouts=1600):
-        mcts = [MCTS(self.envs, self.best_player, temperature=temperature, n_rollouts=n_rollouts, c_puct=self.c_puct)
-                for _ in range(2)]
+        mcts = [
+            MCTS(self.envs, self.best_player, temperature=temperature, n_rollouts=n_rollouts, c_puct=self.c_puct)
+                for _ in range(2)
+        ]
+
         for game in range(n_games):
-            for i in range(2):
-                self.envs[i].reset(i)
-                mcts[i].reset(i)
             history = [[], []]
-
-            done = False
-            while not done:
-                player_turn = self.envs[0].get_player_turn()
-                state = self.envs[player_turn].board.state
-                action, pi = mcts[player_turn].select_action(player_turn)
-                mcts[player_turn].step(action)
-                mcts[1 - player_turn].step((action + 4) % 8)
-                # self.envs[0].print_board()
-
-                _, reward, done = self.envs[player_turn].step(action)
-
-                action_opposite_player_perspective = (action + 4) % 8
-                _ = self.envs[1 - player_turn].step(action_opposite_player_perspective)
-
-                history[player_turn].append([np.squeeze(state), pi])
-
-            for state, pi in history[player_turn]:
-                self.replay_memory.push(state, pi, reward)
-            for state, pi in history[1 - player_turn]:
-                self.replay_memory.push(state, pi, -reward)
-
-            if (game + 1) % (n_games // 10) == 0:
-                print("Completed {}% of self-play.".format(int(100 * (game + 1) / n_games)))
+            winner = play_single_game(self.envs, mcts, history)
+            save_memory(self.replay_memory, winner, history)
+            playing_progress_bar(game, n_games)
 
     def evaluate(self, model, n_games=400, temperature=0.25, new_best_model_threshold=0.55, verbose=0):
         log_every_n_games = n_games // 16
@@ -69,40 +35,64 @@ class Runner(object):
 
         for game in range(n_games):
             starting_player = game % 2
-            for i in range(2):
-                self.envs[i].reset(starting_game=abs(i - starting_player))
-                mcts[i].reset(starting_player)
+            print_results = verbose if verbose and game % log_every_n_games == 0 else 0
 
-            done = False
-            while not done:
-                player_turn = self.envs[0].get_player_turn()
-                action, _ = mcts[player_turn].select_action(player_turn)
-                # if player_turn == 1:
-                #     action = (action + 4) % 8
+            winner = play_single_game(self.envs, mcts, starting_player, verbose=print_results)
 
-                if verbose == 2 and game % log_every_n_games == 0:
-                    self.envs[0].print_board()
-
-                _, reward, done = self.envs[player_turn].step(action)
-
-                action_opposite_player_perspective = (action + 4) % 8
-                _ = self.envs[1 - player_turn].step(action_opposite_player_perspective)
-
-            if verbose == 1 and game % log_every_n_games == 0:
-                self.envs[0].print_board()
-
-            if (player_turn == 0 and reward > 0) or (player_turn == 1 and reward < 0):
+            if winner == 0:
                 n_wins += 1
-                if verbose == 1 and game % log_every_n_games == 0:
-                    print("Player won")
-            elif verbose == 1 and game % log_every_n_games == 0:
-                print("Player lost")
 
-        if verbose == 1:
+        if verbose:
             print("Win ratio of new model = {:.2f}".format(100 * n_wins / n_games))
 
         if n_wins / n_games > new_best_model_threshold:
             self.best_player = model
             return True
+        else:
+            return False
 
-        return False
+
+def play_single_game(envs, mcts, history=None, starting_player=0, verbose=0):
+    for i in range(2):
+        envs[i].reset(starting_game=abs(i - starting_player))
+        mcts[i].reset(starting_player)
+
+    done = False
+    player_turn = None
+    reward = None
+    while not done:
+        if verbose == 2:
+            envs[0].print_board()
+
+        player_turn = envs[0].get_player_turn()
+        state = envs[player_turn].board.state
+        action, pi = mcts[player_turn].select_action(player_turn)
+        mcts[player_turn].step(action)
+        mcts[1 - player_turn].step((action + 4) % 8)
+
+        _, reward, done = envs[player_turn].step(action)
+        action_opposite_player_perspective = (action + 4) % 8
+        _ = envs[1 - player_turn].step(action_opposite_player_perspective)
+
+        if history is not None:
+            history[player_turn].append([np.squeeze(state), pi])
+
+    if (player_turn == 0 and reward > 0) or (player_turn == 1 and reward < 0):
+        winner = 0
+    else:
+        winner = 1
+
+    if verbose:
+        envs[0].print_board()
+        if winner:
+            print("Player won")
+        else:
+            print("Player lost")
+
+    return winner
+
+def save_memory(replay_memory, winner, history):
+    for state, pi in history[winner]:
+        replay_memory.push(state, pi, 1)
+    for state, pi in history[1 - winner]:
+        replay_memory.push(state, pi, -1)
